ea33853a996f8ca763ad4f09f40d1bddd655a969
P1X3L0V4 <annakarpin@gmail.com>
2018-02-18 00:59:40 +0100

    CSS v0.6 + mainNav v0.1

--- imports/MainNavbar.js
+++ imports/MainNavbar.js
@@ -4,15 +4,15 @@ import { Meteor } from 'meteor/meteor';
 import { Link } from 'react-router-dom';
 
 export default ({ userId }) => 
-<nav className="navbar navbar-inverse">
+<nav className="navbar navbar-inverse" id="mainNavbarComponent">
   <div className="container">
     <div className="navbar-header">
-      <a className="navbar-brand" href="/">EduCards</a>
+      <a className="navbar-brand" href="/"> {<i className="far fa-clone"></i>} eduCards</a>
     </div>
     <ul className="nav">
-      <li><Link to="/">Home</Link></li>
+      <li><Link to="/">{<i className="fas fa-home"></i>} Home</Link></li>
       {userId ?
-      	<li><Link to="/admin">Edytor</Link></li>: ''
+      	<li><Link to="/admin">{<i className="far fa-edit"></i>} Edytor</Link></li>: ''
        }
     </ul>
     <ul className="nav">
