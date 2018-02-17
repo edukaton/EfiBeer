d48b0d9ea36fc51d02a6fa3e5184edcbf29600d1
P1X3L0V4 <annakarpin@gmail.com>
2018-02-17 22:18:54 +0100

    CSS v0.6

--- imports/SingleCard.js
+++ imports/SingleCard.js
@@ -7,35 +7,44 @@ export default () =>
             <div className="col-md-2">
             </div>
             <div className="col-md-8">
-                <div className="singleCard">
-                    <div className="singleCardDescription">
-                        <h1 className="singleCardTitle">singleCardTitle</h1>
-                        <p>To jest przykładowa treść</p>
+                <div className="singleCard" ontouchstart="this.classList.toggle('hover');">
+                    
+                    <div className="singleCardflipper">
+                    <div className="singleCardFront">
+                        <div className="singleCardDescription">
+                            <h1 className="singleCardTitle">singleCardTitle</h1>
+                            <p>To jest przykładowa treść</p>
+                        </div>
+
+                        <div className="singleCardTrueFalse">
+                            <button type="submit" className="btn-success">
+                                Prawda <i className="fas fa-thumbs-up"></i>
+                            </button>
+                            <button type="submit" className="btn-danger" >
+                                Fałsz <i className="fas fa-thumbs-down"></i>
+                            </button>
+                        </div>
+
+                        <div className="singleCardABCD">
+                            <button type="submit" className="btn">
+                                (A) Odpowiedź A
+                            </button>
+                            <button type="submit" className="btn" >
+                                (B) Odpowiedź B
+                            </button>
+                            <br />
+                            <button type="submit" className="btn">
+                                (C) Odpowiedź C
+                            </button>
+                            <button type="submit" className="btn">
+                                (D) Odpowiedź D
+                            </button>
+                        </div>
                     </div>
 
-                    <div className="singleCardTrueFalse">
-                        <button type="submit" className="btn-success">
-                            Prawda <i className="fas fa-thumbs-up"></i>
-                        </button>
-                        <button type="submit" className="btn-danger" >
-                            Fałsz <i className="fas fa-thumbs-down"></i>
-                        </button>
+                    <div className="singleCardBack">
+                        BACK_CARD
                     </div>
-
-                    <div className="singleCardABCD">
-                        <button type="submit" className="btn">
-                            (A) Odpowiedź A
-                        </button>
-                        <button type="submit" className="btn" >
-                            (B) Odpowiedź B
-                        </button>
-                        <br />
-                        <button type="submit" className="btn">
-                            (C) Odpowiedź C
-                        </button>
-                        <button type="submit" className="btn">
-                            (D) Odpowiedź D
-                        </button>
                     </div>
 
                 </div>
