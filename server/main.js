479c1f42900b17aabe7c0fe026c38767228cf47b
Mateusz Kardaś <mateusz.kardas@absolvent.pl>
2018-02-18 00:45:04 +0100

    wip

--- server/main.js
+++ server/main.js
@@ -1,7 +1,37 @@
 import { Meteor } from 'meteor/meteor';
-import '../imports/api/Cards';
-import '../imports/api/Tracks';
+import { Tracks } from '../imports/api/Tracks';
+import { Cards } from '../imports/api/Cards';
+import lekcja1 from '../imports/LekcjaNaciagacze';
+import lekcja2 from '../imports/CardContent';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  const isSeeded = Tracks.find({userId: -1}).count() > 1;
+  if (!isSeeded) {
+    seed(lekcja1, 'Lekcja 1');
+    seed(lekcja2, 'Lekcja 2');
+  }
+});
+
+const seed = (lesson, name) => {
+  const trackId = Tracks.insert({
+    name,
+    createdAt: new Date(),
+    userId: -1,
+  });
+  tranasformLesson(lesson).map(question => {
+    Cards.insert(Object.assign(
+      {
+        createdAt: new Date(),
+        trackId, 
+      },
+      question
+    ));
+  });
+}
+
+const tranasformLesson = lesson => lesson.map(question => {
+  if (question.answers) {
+    question.answers = question.answers.sort((a1, a2) => +a2.isCorrect - +a1.isCorrect).map(answer => answer.title).join(',');
+  }
+  return question;
 });
