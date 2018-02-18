914bc32a48b46617d6d3e33158e97a10d89d483d
Tomasz Swirski <tomaszswirski@wp.pl>
2018-02-18 10:03:02 +0100

    Ani zmiany

--- imports/MainNavbar.js
+++ imports/MainNavbar.js
@@ -3,15 +3,16 @@ import { LoginButtons } from 'meteor/okgrow:accounts-ui-react';
 import { Meteor } from 'meteor/meteor';
 import { Link } from 'react-router-dom';
 
-export default ({ userId }) => 
-<nav className="navbar navbar-inverse">
+export default ({ userId }) =>
+<nav className="navbar navbar-inverse" id="mainNavbarComponent">
   <div className="container">
     <div className="navbar-header">
-      <a className="navbar-brand" href="/">EduCards</a>
+      <a className="navbar-brand" href="/"> {<i className="far fa-clone"></i>} eduCards</a>
     </div>
     <ul className="nav">
+      <li><Link to="/">{<i className="fas fa-home"></i>} Home</Link></li>
       {userId ?
-      	<li><Link to="/admin">Edytor</Link></li>: ''
+      	<li><Link to="/admin">{<i className="far fa-edit"></i>} Edytor</Link></li>: ''
        }
     </ul>
     <ul className="nav">
