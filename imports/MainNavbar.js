f810a60b5eadcaed027c3dce5c89d36cc084a92e
P1X3L0V4 <annakarpin@gmail.com>
2018-02-18 10:04:07 +0100

    OLD CSS

--- imports/MainNavbar.js
+++ imports/MainNavbar.js
@@ -3,17 +3,14 @@ import { LoginButtons } from 'meteor/okgrow:accounts-ui-react';
 import { Meteor } from 'meteor/meteor';
 import { Link } from 'react-router-dom';
 
-export default ({ userId }) => 
+export default ({ userId }) =>
 <nav className="navbar navbar-inverse" id="mainNavbarComponent">
   <div className="container">
     <div className="navbar-header">
       <a className="navbar-brand" href="/"> {<i className="far fa-clone"></i>} eduCards</a>
     </div>
     <ul className="nav">
-<<<<<<< HEAD
       <li><Link to="/">{<i className="fas fa-home"></i>} Home</Link></li>
-=======
->>>>>>> d36881791f16776ab943491ffa49ee9df46ff169
       {userId ?
       	<li><Link to="/admin">{<i className="far fa-edit"></i>} Edytor</Link></li>: ''
        }
