4803e4fd2440883a2d17ba388a0543038d84f0eb
P1X3L0V4 <annakarpin@gmail.com>
2018-02-18 01:18:05 +0100

    add/sideNavbar

--- imports/App.js
+++ imports/App.js
@@ -14,6 +14,7 @@ import Index from './Index.js';
 import SingleCard from './SingleCard.js';
 import ListofPaths from './ListofPaths.js';
 import MainNavbar from './MainNavbar.js';
+import SideNavbar from './SideNavbar.js';
 import NotFound from './NotFound';
 import AdminIndex from './admin/Index';
 import AdminCardList from './admin/CardList';
