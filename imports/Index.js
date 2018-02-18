cac14cc0f6845d997d393a85f23fddb85a9be2ef
Tomasz Swirski <tomaszswirski@wp.pl>
2018-02-18 01:26:41 +0100

    new landing page

--- imports/Index.js
+++ imports/Index.js
@@ -6,19 +6,28 @@ import { Link } from 'react-router-dom';
 const Index = ({trackList}) => 
   <div className="landingContainer">
   	<div className="landingCenter">
-	    <h1>eduCards</h1>
-	    <h2>prawdopodobnie najlepsza apka edukacyjna we wszechświecie</h2>
+	    <h1 style={{'font-size': 120}}>EduCards</h1>
+	    <h2>Od nauczycieli dla nauczycieli</h2>
 			<br /><br />
-	    <h3>Wypróbuj przykładową lekcje</h3>
+	    <h3>Zobacz przykładową lekcję!</h3>
 			<br /><br />
+
 			{trackList && trackList.map(track => (
-				<div>
-					<strong>
-						<Link to={`/track/${track._id}`}>
-							{track.name}
-						</Link>
-					</strong>
-				</div>
+  <div className="card" style={{"width": "20rem"}}>
+  <img className="card-img-top" src="../landing02.jpeg" alt="Card image cap" />
+  <div className="card-block">
+    <Link to={`/track/${track._id}`}>
+    <h4 className="card-title" style={{"color": "black"}}>{track.name}</h4>
+    </Link>
+    <p className="card-text" style={{"color": "black"}}>Przejdź interaktywną lekcję ucżacą jak odróżnić prawidłowe Wiadomości od tych fałszywych.</p>
+    <Link to={`/track/${track._id}`}>
+    <span className="btn btn-primary">Zacznij lekcje!</span>
+    </Link>
+    <br />
+    <br />
+  </div>
+
+</div>
 			))}
     </div>
   </div>;
